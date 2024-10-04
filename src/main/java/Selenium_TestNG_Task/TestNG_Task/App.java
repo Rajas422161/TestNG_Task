3e219b86110266904215e943044a23216073755f
Rajas422161 <rajas97srm@gmail.com>
2024-10-04 17:47:29 +0530

    Update App.java

--- src/main/java/Selenium_TestNG_Task/TestNG_Task/App.java
+++ src/main/java/Selenium_TestNG_Task/TestNG_Task/App.java
@@ -1,10 +1,1 @@
-package Selenium_TestNG_Task.TestNG_Task;
 
-/**
- * Hello world!
- */
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
